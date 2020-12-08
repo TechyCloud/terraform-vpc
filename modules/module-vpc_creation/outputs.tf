0da9998e5889b0476eb9066029e573e0ad610a2d
root <root@DESKTOP-GEDJ98H.localdomain>
2020-12-08 12:14:25 +0530

    Updated Output.tf file

--- modules/module-vpc_creation/outputs.tf
+++ modules/module-vpc_creation/outputs.tf
@@ -5,8 +5,28 @@ output "VPC_ID" {
 output "public" {
   value       = "${aws_subnet.public.*.id}"
   description = "The ID of the Public Subnets"
-} 
+}
 output "private" {
   value       = "${aws_subnet.private.*.id}"
   description = "The ID of the Private Subnets"
+}
+output "VPC-Peering" {
+  value = "${aws_vpc_peering_connection.VPC-Peering.id}"
+  description = "VPC Peering ID"
+}
+output "IGW" {
+  value = "${aws_internet_gateway.IGW.id}"
+  description = "Internet Gateway ID"
+}
+output "Route-table-IG-attached" {
+  value = "${aws_default_route_table.Route-table-IG-attached.id}"
+  description = "Public Route Table ID"
+}
+output "private-rt" {
+  value = "${aws_route_table.private-rt.id}"
+  description = "Private Route Table ID"
+}
+output "ElasticIP_NGW" {
+  value = "${aws_eip.ElasticIP_NGW.id}"
+  description = "Elastic IP Address"
 }
